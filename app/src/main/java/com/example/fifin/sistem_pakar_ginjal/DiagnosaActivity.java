56250dd1819db03f1d51b893240e0f8852277424
slamet fifin alamsyah <s.fifinalamsyah@gmail.com>
2019-06-04 14:13:07 +0700

    menambahkan activity info, pakar, solusi, myapp

--- app/src/main/java/com/example/fifin/sistem_pakar_ginjal/DiagnosaActivity.java
+++ app/src/main/java/com/example/fifin/sistem_pakar_ginjal/DiagnosaActivity.java
@@ -2,6 +2,7 @@ package com.example.fifin.sistem_pakar_ginjal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,6 +23,7 @@ public class DiagnosaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_diagnosa);
         lvGejala=(ListView)findViewById(R.id.lv_gejala);
         lvGejala.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
 
         String[] items ={"gejala 1","gejala 2"};
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.row_checkbox,R.id.cb_tv_gejala,items);
